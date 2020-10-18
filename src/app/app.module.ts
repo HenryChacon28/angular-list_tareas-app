2ef8a7ec47ed9e919b8c98e9cd17b0b2b6025cef
Henry <Henrychaconrm@gmail.com>
2020-10-17 18:17:44 -0600

    commit para subir repositorio

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// Rutas
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ListTareasComponent } from './dashboard/list-tareas/list-tareas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ListTareasComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
